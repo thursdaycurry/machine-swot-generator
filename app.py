4a95c39c834fc5f4ffb7f54957befe4825f19aff
thursdaycurry <thursdaycurry@gmail.com>
2022-11-23 21:48:18 +0900

    working on index.js

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
-
+# For Private Key
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 PRIVATE_KEY = os.environ.get("PRIVATE_KEY")
-print(PRIVATE_KEY)
+
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/swot', methods=["POST"])
+def swot_post():
+    return jsonify({'msg': '등록 완료!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
